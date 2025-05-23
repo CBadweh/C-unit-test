b5698bdd27deec1c851c8ea59780595b782c927d
C Badweh <sheenaymudwe@gmail.com>
2025-05-23 09:21:29 -0400

    Add negative_values_test unit test

--- src/average/average.c
+++ src/average/average.c
@@ -1,5 +1,7 @@
 #include "average.h"
 
+// Correctly Implement average(), all unit test cases will pass
+/*
 float average(float arr[], unsigned int size)
 {
     if (size == 0)
@@ -8,10 +10,68 @@ float average(float arr[], unsigned int size)
     }
     
     float total = 0;
-    for (unsigned int i = 0; i < size; i++)
+    for (unsigned int i = 0; i <= size; i++) 
     {
         total += arr[i];
     }
     
     return total / (float)size;
 }
+*/
+
+
+// Break The Function to observe simple_test. Replace  with *
+/*
+float average(float arr[], unsigned int size)
+{
+    if (size == 0)
+    {
+        return 0.0;
+    }
+    
+    float total = 0;
+    for (unsigned int i = 0; i <= size; i++) 
+    {
+        total += arr[i];
+    }
+    
+    return total * (float)size; // ❌ wrong! should divide, not multiply
+}
+*/
+
+// Break The Function to observe null_test
+/*
+float average(float arr[], unsigned int size)
+{
+    // BUG: No check for size == 0
+    float total = 0;
+    for (unsigned int i = 0; i < size; i++)
+    {
+        total += arr[i];
+    }
+
+    return total / (float)size; // ❌ Division by 0 if size == 0
+}
+*/
+
+
+
+// negative_value_tests
+/*
+float average(float arr[], unsigned int size)
+{
+    if (size == 0)
+    {
+        return 0.0;
+    }
+    
+    float total = 0;
+    for (unsigned int i = 0; i <= size; i++) 
+    {
+        // Hidden bug: skips negatives
+        if (arr[i] >= 0) total += arr[i];
+    }
+    
+    return total / (float)size;
+}
+    */
--- tests/average/test.cpp
+++ tests/average/test.cpp
@@ -29,7 +29,14 @@ TEST(average_test_group, simple_test)
 // Test null array
 TEST(average_test_group, null_test)
 {
-    float array[1];
-    float avg = average(array, 0);
-    CHECK_EQUAL(avg, 0.0);
+    float array[1];                // Content is irrelevant
+    float avg = average(array, 0); // Call with size = 0
+    CHECK_EQUAL(avg, 0.0);         // Expect: return value is 0.0
+}
+
+TEST(average_test_group, negative_values_test)
+{
+    float array[] = {-2.0, -4.0, -6.0, -8.0};
+    float avg = average(array, 4);
+    DOUBLES_EQUAL(-5.0, avg, 0.0001);  // Expect: (-2 -4 -6 -8)/4 = -5
 }
